2d8cfb1ebe428f0bbd762476f838acd8d63fb3a5
Thach Le Nguyen <thach.lenguyen@insight-centre.org>
2018-01-27 22:01:33 +0000

    remove sh scripts

--- scripts/seql_multi.sh
+++
@@ -1,1 +0,0 @@
-./seql_multiclass data/$1_TRAIN data/$1_TEST multi $2 $3 $4
--- scripts/testsp.sh
+++
@@ -1,3 +0,0 @@
-./seql_learn -n 1 -v 1 data/test_TRAIN seql/seql.model 
-./seql_mkmodel -i seql/seql.model -o seql/seql.model.bin -O seql/seql.predictor 
-./seql_classify -n 1 -v 4 -w 4 data/test_TEST seql/seql.model.bin 
